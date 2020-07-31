81988f558beef6d7e24bfc8019d76ba9292eb1c3
QuantumCalzone <QuantumCalzone@gmail.com>
2020-07-30 23:16:20 -0700

    Update Utilities.cs

--- Packages/UnityBookmarks/Editor/Utilities.cs
+++ Packages/UnityBookmarks/Editor/Utilities.cs
@@ -28,10 +28,33 @@ namespace QuantumCalzone
             }
         }
 
+        public static T GetOrCreate<T>(string subDirectory = "") where T : ScriptableObject
+        {
+            var allInstances = GetAllInstances<T>();
+            if (allInstances.Length == 0)
+            {
+                var newInstance = ScriptableObject.CreateInstance(typeof(T)) as T;
+                AssetDatabase.CreateAsset(newInstance, string.Format("{0}.asset", Path.Combine("Assets", subDirectory, typeof(T).Name)));
+                AssetDatabase.SaveAssets();
+                return newInstance;
+            }
+            else if (allInstances.Length > 1)
+            {
+                Debug.LogError(string.Format("Sorry! This only supports 1 Bookmark asset. Please remove {0} of the ones logged below so that only 1 remains.", allInstances.Length - 1));
+                for (var i = 0; i < allInstances.Length; i++)
+                {
+                    Debug.Log(allInstances[i].name, allInstances[i]);
+                }
+            }
+
+            return allInstances[0];
+        }
+
         public static T[] GetAllInstances<T>() where T : ScriptableObject
         {
-            var guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);  //FindAssets uses tags check documentation for more info
-            T[] a = new T[guids.Length];
+            var guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T).Name));  //FindAssets uses tags check documentation for more info
+            var a = new T[guids.Length];
+
             for (var i = 0; i < guids.Length; i++)
             {
                 var path = AssetDatabase.GUIDToAssetPath(guids[i]);
@@ -40,7 +63,7 @@ namespace QuantumCalzone
 
             return a;
         }
-
+        
         public static void DrawLabelCenteredBold(string s)
         {
             EditorGUILayout.BeginHorizontal();
